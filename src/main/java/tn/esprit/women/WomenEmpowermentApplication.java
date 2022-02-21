de80137ca2dfcc2708a21d785182dd97268b5331
oumaima5320 <oumaimabensouissi@gmail.com>
2022-02-21 22:33:39 +0100

    First commit

---
+++ src/main/java/tn/esprit/women/WomenEmpowermentApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.women;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomenEmpowermentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomenEmpowermentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/women/WomenEmpowermentApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.women;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WomenEmpowermentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
